20a547aaf9462d674931113332e9e2f249f534ee
gaffelmagasin <168209504+gaffelmagasin@users.noreply.github.com>
2024-07-29 13:27:49 +0200

    Update scripts_ab.js

--- scripts_ab.js
+++ scripts_ab.js
@@ -43,7 +43,7 @@ function open_article(str){
 				var new_html = ``
 
 				//bild
-				new_html += `<div style = "width:100%; height: auto; display: table; margin-bottom: 10px; postion: relative;">
+				new_html += `<div style = "width:100%; height: auto; display: table; margin-bottom: 10px; position: relative;">
 					<img src = ${bild} id = ${'bild' + div_id} 
      						style = "width: ${imgw}%; float: right; background: #000000FF;">
 	   				</img>`
