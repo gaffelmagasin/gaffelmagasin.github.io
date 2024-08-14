391da6eba5522ea6b0cd5ad3c3162e6a634403e7
gaffelmagasin <168209504+gaffelmagasin@users.noreply.github.com>
2024-08-14 14:56:18 +0200

    Update scripts_ab.js

--- scripts_ab.js
+++ scripts_ab.js
@@ -32,7 +32,8 @@ function open_article(str){
 				var text_style_preamble = `font-size: clamp(5px,2vw,20px);`
 				var text_style_writer = `clamp(5px,2vw,20px)`
 				var imgw = 53;
-				var roww = 30;
+				var roww = 38;
+				if(porträtt != "-1"){roww = 30;}
 				if(small == true){
 					imgw = 100;
 					if(portrait != "-1"){roww = 68;}else{roww = 100;}
